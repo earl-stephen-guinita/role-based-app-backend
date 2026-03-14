ea99c8aeaafd126af38f54adb73237aa927783f7
EstebanBraun <guinitaearlstephen@gmail.com>
2026-03-14 21:31:04 +0800

    updated server.js by adding post route for login

--- server.js
+++ server.js
@@ -47,3 +47,19 @@ app.post('/api/register', async (req, res) => {
    res.status(201).json ({ message: 'User registered', username, role});
 });
 
+app.post('/api/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = users.find(u => u.username === username);
+    if (!user || !await bcrypt.compare(password, user.password)) {
+        return res.status(401).json({ error: 'Invalud credentials' });
+    }
+
+    const token = jwt.sign(
+        { id: user.id, username: user.username, role: user.role },
+        SECRET_KEY,
+        { expiresIn: '1h'}
+    );
+
+    res.json({ token, user: { username: user.username, role: user.role } });
+});
