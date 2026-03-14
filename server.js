e631cab6708442c5f2ca4df9ca91c595caefd386
EstebanBraun <guinitaearlstephen@gmail.com>
2026-03-14 23:18:50 +0800

    updated server.js by adding role authorization

--- server.js
+++ server.js
@@ -90,3 +90,12 @@ function authenticateToken(req, res, next) {
         next();
     });
 }
+
+function authorizeRole(role) {
+    return (req, res, next) => {
+        if (req.user.role !== role) {
+            return res.status(403).json({ error: 'Access denied: insufficient permissions' });
+        }
+        next();
+    };
+}
