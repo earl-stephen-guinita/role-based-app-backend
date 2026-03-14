fc05e8666ace369f2d10e081d004362be5c7d16c
EstebanBraun <guinitaearlstephen@gmail.com>
2026-03-14 23:14:39 +0800

    updated server.js by adding token authentication

--- server.js
+++ server.js
@@ -76,3 +76,17 @@ app.get('/api/content/guest', (req, res) => {
     res.json({ message: 'Public content for all visitors' });
 });
 
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if(!token) {
+        return res.status(401).json({ error: 'Access token required' });
+    }
+
+    jwt.verify(token, SECRET_KEY, (err, user) => {
+        if (err) return res.status(403).json({ error: 'Invalid or expired token' });
+        req.user = user;
+        next();
+    });
+}
